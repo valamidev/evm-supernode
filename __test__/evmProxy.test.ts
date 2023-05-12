4a942f3443e0c7a23770390743b1db2a7db4d84a
T <52665225+valamidev@users.noreply.github.com>
2023-05-12 11:51:46 +0200

    CleanUp

--- __test__/evmProxy.test.ts
+++ __test__/evmProxy.test.ts
@@ -1,12 +1,4 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
 import { ethers } from "ethers";
-
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
-
-beforeEach(() => {
-  process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
-});
 
 describe("Evm Proxy Test", () => {
   it("Highest block", async () => {
