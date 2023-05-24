33b8d8a24239dadae3763c98b4322bb4c54cf3db
T <52665225+valamidev@users.noreply.github.com>
2023-05-24 09:01:13 +0200

    Bump

--- src/common/chainData/handlers/chainRpcList.ts
+++ src/common/chainData/handlers/chainRpcList.ts
@@ -20,4 +20,27 @@ export const fetchExtraRpcs = async () => {
   } catch (error) {
     console.error(error);
   }
+
+  // Fallback to last working version
+  try {
+    const response = await fetch(
+      "https://raw.githubusercontent.com/DefiLlama/chainlist/b9857beb81d48c041ad906a6bb51e4b36fc58186/constants/extraRpcs.js"
+    );
+    const data = await response.text();
+
+    const regex = /export const extraRpcs = ({[\s\S]*});/;
+    const match = data.match(regex);
+
+    if (match) {
+      var privacyStatement = {};
+
+      const rpcList = eval("(" + match[1] + ")");
+
+      return rpcList;
+    } else {
+      console.log("Extra RPCs not found in file.");
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
--- src/common/chainData/handlers/chainlist.ts
+++ src/common/chainData/handlers/chainlist.ts
@@ -9,4 +9,17 @@ export const fetchChainIds = async () => {
   } catch (error) {
     console.error(error);
   }
+
+  // Fallback to last working version
+
+  try {
+    const response = await fetch(
+      "https://raw.githubusercontent.com/DefiLlama/chainlist/5e2fa1ff401f5e66ebd02b3828da7180c1a7e2f3/constants/chainIds.json"
+    );
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
